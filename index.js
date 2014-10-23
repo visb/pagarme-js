79cec5627401207de4cc6ed3fc746a360e8e59db
Vinicius Borges <visb42@gmail.com>
2014-10-23 17:00:11 -0200

    fixing some bugs

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var pagarme = require('./lib/pagarme')('testeApiKey');
+var pagarme = require('./lib/pagarme')('ak_test_jCoHaG0wvbpJnBtafcRbEvJ42QmzZw');
 
-// pagarme.apiKey = 'outraApiKey';
+// pagarme.apiKey = '';
 
 var transaction = pagarme.transaction.create({
-    testKey: 'testValue'
+    card_number: '4901720080344448',
+    card_holder_name: 'Usuario de Testes',
+    card_expiration_date: '0216',
+    card_cvv: '314',
+    amount: '1000'
 });
 
 transaction.charge(function(error, response) {
-    // ...
+    console.log(arguments);
 });
 
