cfd857c3ef9794a25142e5fa2e22525038997935
Vinicius Borges <visb42@gmail.com>
2014-10-26 18:55:41 -0200

    fixing requests issue

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ var pagarme = require('./lib/pagarme')('ak_test_jCoHaG0wvbpJnBtafcRbEvJ42QmzZw')
 var transaction = pagarme.transaction.create({
     card_number: '4901720080344448',
     card_holder_name: 'Usuario de Testes',
-    card_expiration_date: '0216',
+    card_expiration_year: '16',
+    card_expiration_month: '02',
     card_cvv: '314',
     amount: '1000'
 });
 
 transaction.charge(function(error, response) {
-    console.log(arguments);
+	if (error) {
+		return console.log(error);
+	}
+
+	console.log(response);
 });
 
