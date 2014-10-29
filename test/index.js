3a4998449e6ac0d8bf4694a0eeb538d61286cfca
Vinicius Borges <visb42@gmail.com>
2014-10-28 23:15:58 -0200

    enable transactions to search by anything

---
+++ test/index.js
@@ -0,0 +1,5 @@
+/**
+ * Apenas prototipo dos testes, não está configurado para rodar,
+ * estão aqui apenas para implementação futura
+ */
+var pagarme = require('./lib/pagarme')('ak_test_jCoHaG0wvbpJnBtafcRbEvJ42QmzZw');
--- test/transaction.js
+++ test/transaction.js
@@ -1,16 +1,53 @@
+/**
+ * Apenas prototipo dos testes, não está configurado para rodar,
+ * estão aqui apenas para implementação futura
+ */
 describe('be able to charge', function() {
+        
+    var transaction = pagarme.transaction.create({
+        card_number: '4901720080344448',
+        card_holder_name: 'Usuario de Testes',
+        card_expiration_year: '16',
+        card_expiration_month: '02',
+        card_cvv: '314',
+        amount: '1000'
+    }, function(err, response) {
+        if (err) return console.log(err);
+
+        console.log('Created transaction #' + response.id);
+    });
     
 });
 
 describe('be able to refund', function() {
     
+    pagarme.transaction.refund(response.id, function(err, response){
+        if (err) return console.log(err);
+        console.log('Refunded transaction #' + response.id);
+    });
+    
 });
 
 describe('be able to search by anything', function() {
     
+    pagarme.transaction.get({amount: 1000, status: 'paid'}, function(err, response) {
+        for (i in response) {
+            var item = response[i];
+            console.log('Transaction #' + item.id + ' ' + item.status);
+        }
+    });
+    
 });
 
 describe('be able to create transaciton with boleto', function() {
+    
+    var transaction = pagarme.transaction.create({
+        payment_method: 'boleto',
+        amount: '1000'
+    }, function(err, response) {
+        if (err) return console.log(err);
+        console.log('Created transaction #' + response.id);
+    });
     
 });
 
@@ -19,6 +56,10 @@ describe('be able to send metadata', function() {
 });
 
 describe('be able to find a transaction', function() {
+    
+    pagarme.transaction.get(155808, function(err, response) {
+        console.log('Transaction #' + resoibse.id);
+    });
     
 });
 
