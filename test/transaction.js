fcaca21a30cfd92cc21c5588f9ca13ceee13ec48
Vinicius Borges <visb42@gmail.com>
2014-11-24 18:27:30 -0200

    be able to charge transaction

--- test/transaction.js
+++ test/transaction.js
@@ -1,15 +1,17 @@
 var Pagarme = require('../lib/pagarme');
 
-var transaction = new Pagarme.Transaction({
-  card_number: '4901720080344448',
-  card_holder_name: 'Usuario de Testes',
-  card_expiration_year: '16',
-  card_expiration_month: '02',
-  card_cvv: '314',
-  amount: '1000'
-});
+describe('be able to charge', function(){
+  var transaction = new Pagarme.Transaction({
+    card_number: '4901720080344448',
+    card_holder_name: 'Usuario de Testes',
+    card_expiration_year: '16',
+    card_expiration_month: '02',
+    card_cvv: '314',
+    amount: '1000'
+  });
 
-transaction.charge(function() {
-  console.log(arguments);
-  console.log(transaction);
+  transaction.charge(function() {
+    console.log(transaction.id);
+    console.log(this.id);
+  });
 });
