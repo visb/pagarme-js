47771f71a02b95a0a7f7fd371090939651be807f
Vinicius Borges <visb42@gmail.com>
2014-11-25 08:30:17 -0200

    be able to search & list

--- test/transaction.js
+++ test/transaction.js
@@ -12,8 +12,12 @@ describe('be able to charge', function() {
 
   transaction.charge(function(error) {
     if (error) return console.log(error);
+
     console.log(this.id);
     console.log(this.status); // paid
+
+    // tests to do
+    // this.status = paid
   });
 });
 
@@ -25,8 +29,12 @@ describe('be able to create transaciton with boleto', function() {
 
   transaction.charge(function(error) {
     if (error) return console.log(error);
+
     console.log(this.id);
     console.log(this.status); // wayting_payment
+
+    // tests to do
+    // this.status = waytinh_payment
   });
 });
 
@@ -44,9 +52,14 @@ describe('be able to refund', function() {
     if (error) return console.log(error);
 
     console.log(this.id);
+
     this.refund(function(error) {
       if (error) return console.log(error);
+
       console.log(this.status); // refunded
+
+      // tests to do
+      // this.status = refunded
     });
   });
 });
@@ -54,5 +67,32 @@ describe('be able to refund', function() {
 describe('be able to find a transaction', function() {
   Pagarme.Transaction().get(168927, function() {
     console.log(this.status);
+
+    // tests to do
+    // this.id = 168927
   });
+});
+
+describe('be able to list', function() {
+  Pagarme.Transaction().all(function(errors) {
+    if (errors) return console.log(errors);
+
+    console.log(this);
+
+    // tests to do
+    // this.length <= 5
+  }, 1, 5);
+});
+
+
+describe('be able to search by anything', function() {
+  Pagarme.Transaction().find({status: 'refunded'}, function(errors) {
+    if (errors) return console.log(errors);
+
+    console.log(this);
+
+    // tests to do
+    // this.length <= 5
+    // this.forEach(each.status = refunded)
+  }, 1, 5);
 });
