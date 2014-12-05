6a974505ec2a05da9f20f1721dae74b1421ec877
Vinicius Borges <visb42@gmail.com>
2014-12-05 16:44:17 +0000

    config test env

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('test', function() {
+  return gulp.src('test/index.js', {read: false}).pipe(mocha());
+});
---
+++ test/index.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+describe('describe', function() {
+  it('it', function(done) {
+    expect(2).to.equal(2);
+    done();
+  });
+});
